82f7af2d8f2dd245e04fe56cc4a08c9827a27341
Pawel Lbs <pawel.lbs@gmail.com>
2019-05-16 23:40:28 +0200

    scripts and tidying

---
+++ scripts/custom-utils.js
@@ -0,0 +1,27 @@
+const { exec } = require('child_process');
+
+function executionPromise(command) {
+  return new Promise((resolve, reject) => {
+    const executor = exec(command, (err, stdout, stderr) => {
+      if (stdout) resolve(stdout);
+    });
+
+    executor.stdout.on('data', console.log);
+    executor.stderr.on('data', console.log);
+    executor.stderr.on('exit', code => console.log('EXIT', code));
+  });
+}
+
+function getArguments() {
+  return process.argv.slice(2).join(' ');
+}
+
+function applyTerminalArguments(command) {
+  const args = getArguments();
+  return args ? `${ command } ${ args }` : command;
+}
+
+module.exports = {
+  executionPromise,
+  applyTerminalArguments
+};
