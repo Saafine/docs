523da4e6e9b49e1d48eb8556efe839101fc1ba45
Pawel Labus <pawel.labus@xtb.com>
2019-07-26 19:54:01 +0200

    x

--- js-utils/filtering arrays.js
+++
@@ -1,4 +0,0 @@
-/**
- * This will filter coins object to return array with only 1 element: object with .name property equal to "coinName"
- */
-const coinData = this.coins.filter((_coin) => _coin.name === coinName)[0];
--- js-utils/nulling keys.js
+++
@@ -1,7 +0,0 @@
-for (let key in this.modal ) {
-  if (key === 'open') {
-	this.modal[key] = false;
-  } else {
-	this.modal[key] = null;
-  }
-}
