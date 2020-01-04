645b48392ed7017cdf3abbe2a632ce91e2bb3176
Pawel Labus <pawel.labus@xtb.com>
2020-01-04 13:12:27 +0100

    x

--- algorithms/043-equal-circle-segments.ts
+++ algorithms/043-equal-circle-segments.ts
@@ -32,74 +32,53 @@ function round(value) {
     return Math.round(value * 10000) / 10000
 }
 
-
-function getMaxRadiusAndTotalArea(radii) {
-    return radii.reduce((acc, val) => {
-        let [biggestCircleArea, totalArea] = acc;
-        const area = getCircleArea(val);
-
-        if (biggestCircleArea < area) {
-            biggestCircleArea = area;
-        }
-
-        return [biggestCircleArea, totalArea + getCircleArea(val)];
-    }, [0, 0]);
-}
-
-function getAreas(radii) {
-    return radii.map(getCircleArea);
-}
-
 function largestSegment(radii, segments) {
-    radii.sort((a, b) => b - a);
-    const [, totalArea] = getMaxRadiusAndTotalArea(radii);
-    const segMax = totalArea / segments;
-    console.log(segments);
-    console.log(segMax);
-    console.log(totalArea);
-    console.log(getAreas(radii));
-    const trySeg = segMax;
-    // console.log(totalArea);
+    const areas = radii.sort((a, b) => b - a).map(getCircleArea);
 
+    console.log(areas);
+    console.log(segments);
     // we can filter all radiuses smaller then seg min
-    return getOptimalSegment(getAreas(radii), segments, trySeg).toFixed(4)
+    return getOptimalSegment(areas, segments, areas[0]).toFixed(4)
 }
 
 // try seg initially segMax
 function getOptimalSegment(areas, segments, trySeg, safety = 0) {
-    // if (safety > 1) throw Error('Safety check');
-
-    // if (safety > 2) {
-    //     console.log(trySeg);
-    // }
+    if (safety === 2) return;
     let segmentsLeft = segments;
     let tryNext = 0;
+
     for (let x = 0; x < areas.length; x++) {
-        const segmentsOnArea = round(areas[x] / trySeg);
-        const reducedSegments = Math.floor(segmentsOnArea);
-        // console.log(safety);
-        // if (safety === 0) {
-        //     console.log(areas[x] / trySeg);
-        //     console.log(round(areas[x] / trySeg));
-        //     trySeg
-        //     console.log(reducedSegments);
-        // }
-        segmentsLeft = segmentsLeft - reducedSegments;
-        // console.log(segmentsLeft);
+        if (trySeg > areas[x]) {
+            tryNext = tryNext > areas[x] ? tryNext : areas[x];
+            break;
+        }
+
+        const segmentsOnArea = fitSegments(areas[x], trySeg);
+        console.log(segmentsOnArea);
+        segmentsLeft = segmentsLeft - segmentsOnArea;
         if (segmentsLeft <= 0) return trySeg;
 
-        const tryNextOne = areas[x] / Math.ceil(segmentsOnArea) - 1;
-        const tryNextOne2 = areas[x] / Math.ceil(segmentsOnArea);
-        const tryNextTemp = areas[x] % trySeg ? tryNextOne2 : tryNextOne;
-        // console.log({tryNextOne, tryNextOne2, tryNextTemp});
-
-        tryNext = tryNextTemp > tryNext ? tryNextTemp : tryNext;
+        // const tryNextOne = areas[x] / Math.ceil(segmentsOnArea) - 1;
+        // const tryNextOne2 = areas[x] / Math.ceil(segmentsOnArea);
+        // const tryNextTemp = areas[x] % trySeg ? tryNextOne2 : tryNextOne;
+        // tryNext = tryNextTemp > tryNext ? tryNextTemp : tryNext;
+        // console.log(tryNext);
     }
 
+    if (safety === 1) {
+        console.log(areas , tryNext);
+
+    }
     return getOptimalSegment(areas, segments, tryNext, ++safety);
 }
 
-trySolution(largestSegment, testData, 3);
+function fitSegments(area, trySeg) {
+    const segmentsOnArea = round(area / trySeg);
+    const reducedSegments = Math.floor(segmentsOnArea);
+    return reducedSegments;
+}
+
+trySolution(largestSegment, testData, 1);
 
 function trySolution(solutionFn, cases, specifyIdx = undefined) {
     let casesLen = cases.length;
