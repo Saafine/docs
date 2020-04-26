aa2a764e1651833a13bd02e8cbc949971e49a446
Labus, Pawel <pawel.labus@santander.pl>
2020-04-26 09:52:12 +0200

    X

--- algorithms/068-Search-In-Rotated-Sorted-Array.ts
+++ algorithms/068-Search-In-Rotated-Sorted-Array.ts
@@ -12,34 +12,9 @@ const meta = {
 };
 
 const testData = [
-    // {
-    //     args: [[0, 1, 2, -2, -1], -1],
-    //     output: 4
-    // },
-    // {
-    //     args: [[4, 5, 6, 7, 0, 1, 2], 0],
-    //     output: 4
-    // },
-    // {
-    //     args: [[4, 5, 6, 7, 0, 1, 2], 3],
-    //     output: -1
-    // },
-    // {
-    //     args: [[6, 7, 0, 1, 2, 4, 5], 7],
-    //     output: 1
-    // },
-    // {
-    //     args: [[6, 7, 2, 3, 4, 5], 2],
-    //     output: 2
-    // },
-    // {
-    //     args: [[8, 9, 2, 3, 4, 5, 6, 7], 2],
-    //     output: 2
-    // }
-
     {
         args: [[0, 1, 2, -2, -1], -1],
-        output: 3
+        output: 4
     },
     {
         args: [[4, 5, 6, 7, 0, 1, 2], 0],
@@ -47,11 +22,11 @@ const testData = [
     },
     {
         args: [[4, 5, 6, 7, 0, 1, 2], 3],
-        output: 4
+        output: -1
     },
     {
         args: [[6, 7, 0, 1, 2, 4, 5], 7],
-        output: 2
+        output: 1
     },
     {
         args: [[6, 7, 2, 3, 4, 5], 2],
@@ -62,43 +37,100 @@ const testData = [
         output: 2
     },
     {
-        args: [[1, 7], 0],
-        output: 0
-    },
-    {
-        args: [[7, 1], 0],
-        output: 1
-    },
+        args: [[4,5,6,7,0,1,2], 0],
+        output: 4
+    }
+
+    // testing binarySearchOfStartIndexOfUnsortedArray
+    // {
+    //     args: [[0, 1, 2, -2, -1], -1],
+    //     output: 3
+    // },
+    // {
+    //     args: [[4, 5, 6, 7, 0, 1, 2], 0],
+    //     output: 4
+    // },
+    // {
+    //     args: [[4, 5, 6, 7, 0, 1, 2], 3],
+    //     output: 4
+    // },
+    // {
+    //     args: [[6, 7, 0, 1, 2, 4, 5], 7],
+    //     output: 2
+    // },
+    // {
+    //     args: [[6, 7, 2, 3, 4, 5], 2],
+    //     output: 2
+    // },
+    // {
+    //     args: [[8, 9, 2, 3, 4, 5, 6, 7], 2],
+    //     output: 2
+    // },
+    // {
+    //     args: [[1, 7], 0],
+    //     output: 0
+    // },
+    // {
+    //     args: [[7, 1], 0],
+    //     output: 1
+    // },
+    // {
+    //     args: [[0, 1, 2, 3, -1], 0],
+    //     output: 4
+    // },
 ];
 
+function binarySearchOfStartIndexOfUnsortedArray(arr, left = 0, right = arr.length - 1, previous = arr[0]) {
+    if (left > right) return -1;
+    const midIndex = Math.floor((left + right) / 2);
+    const mid = arr[midIndex];
+    const previousValue = arr[midIndex - 1];
+    if (previousValue > mid) {
+        return midIndex;
+    }
 
-function findStartIndex(arr, index = 0, min = 0, max = arr.length - 1) {
-    const value = arr[index];
-    const next = arr[index + 1];
-    if (value > next) return index;
-    if (index === 0) return findStartIndex(arr, Math.floor(max - (max - index) / 2), min, max);
-
-    const minValue = arr[min];
-    // go right
-    if (value > minValue) {
-        min = index;
-        const nextIndex = Math.floor(max - (max - index) / 2);
-        return findStartIndex(arr, nextIndex, min, max);
+    if (previous <= mid) {
+        return binarySearchOfStartIndexOfUnsortedArray(arr, midIndex + 1, right, mid);
     } else {
-        // go left
-        max = index;
-        const nextIndex = Math.floor(index - (index - min) / 2);
-        return findStartIndex(arr, nextIndex, min, max);
+        return binarySearchOfStartIndexOfUnsortedArray(arr, left, midIndex - 1);
     }
 }
 
-function solution(arr, target) {
-    if (!arr.length) return -1;
-    const idx = findStartIndex(arr);
-    return idx;
+function binarySearch(arr, target, left = 0, right = arr.length - 1) {
+    if (left > right) return -1;
+
+    // Depending if you use ceil/floor, you may get a different result if your array looks for instance like: 0 1 1 1 1 2 and looking for the index of a 1
+    const midIndex = Math.floor((left + right) / 2);
+    const mid = arr[midIndex];
+    if (mid === target) return midIndex;
+
+    if (mid <= target) {
+        // go right
+        return binarySearch(arr, target, midIndex + 1, right);
+    } else {
+        // go left
+        return binarySearch(arr, target, left, midIndex - 1);
+    }
 }
 
-trySolution(solution, testData);
+
+function search(arr, target) {
+    if (!arr.length) return -1;
+    const isSorted = arr[0] < arr[arr.length - 1];
+    const idx = isSorted ? 0 : binarySearchOfStartIndexOfUnsortedArray(arr);
+
+    const leftSorted = arr.slice(0, idx);
+    const searchLeftSorted = binarySearch(leftSorted, target);
+    if (searchLeftSorted !== -1) return searchLeftSorted;
+
+    const rightSorted = arr.slice(idx);
+    const searchRightSorted = binarySearch(rightSorted, target);
+    if (searchRightSorted !== -1) return leftSorted.length + searchRightSorted
+
+    return -1;
+}
+
+trySolution(search, testData);
 
 function trySolution(solutionFn, cases, specifyIdx = undefined) {
     let casesLen = cases.length;
