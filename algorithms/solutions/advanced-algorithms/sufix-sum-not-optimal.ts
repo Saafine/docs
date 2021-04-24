10adc2701c057de595cfe55c5f521c1fe2659ffe
Labus, Pawel <pawel.labus@falkbuilt.com>
2021-04-24 17:10:45 +0200

    Prefix/sufix alg

--- algorithms/solutions/advanced-algorithms/sufix-sum-not-optimal.ts
+++ algorithms/solutions/advanced-algorithms/sufix-sum-not-optimal.ts
@@ -1,10 +1,9 @@
-// https://i.imgur.com/SFCGye8.png
+// https://i.imgur.com/LLV0PHu.png
 // Given [2, 1, 3, 2, 1, 2, 1, 2]
-// Return [2, 3, 6, 8, 9, 11, 12, 14]
-// Given index, returns sum of elements to that index, for example: i = 2 -> 2 + 1 + 3 = 6
+// Return
 
 // const A = ['X', 2, 1, 3, 2, 1, 2, 1, 2];
-const A = ['X', 2,1,3,2,1,3,4,5];
+const A = ['X', 1, 1, 2, 2, 3, 8, 7, 3];
 let B = [];
 
 const n = A.length - 1;
@@ -25,25 +24,25 @@ const processorIndexToOperationsCount = {};
 
 for (let h = 1; h <= logn; h++) {
     let TEMP_B = [...B];
-    for (let i = Math.pow(2, h - 1) + 1; i <= n; i++) { // tutaj musialem dodac 1 do i, żeby zaczynało od następnej liczby
-        if (h === 2) {
+    for (let i = 1; i <= n - Math.pow(2, h - 1); i++) { // tutaj musialem dodac 1 do i, żeby zaczynało od następnej liczby
+        if (h === 1) {
             // Zad 1
-            console.log(`W tym kroku Dzialaja procesory o identyfikatorach ${Math.pow(2, h - 1) + 1}:${n}`);
-            console.log(`Ilosc dzialajacych procesorow w tym kroku: ${n - (Math.pow(2, h - 1))}`);
-            console.log(`Krok ${h}: ${ B[i - Math.pow(2, h - 1)] } + ${ B[i] }`);
+            // console.log(`W tym kroku Dzialaja procesory o identyfikatorach 1:${n - Math.pow(2, h - 1)}`);
+            // console.log(`Ilosc dzialajacych procesorow w tym kroku: ${n - Math.pow(2, h - 1) + 1}`);
+            // console.log(`Krok ${h}: ${ B[i + Math.pow(2, h - 1)] } + ${ B[i] }`);
         }
-        TEMP_B[i] = B[i - Math.pow(2, h - 1)] + B[i];
+        TEMP_B[i] = B[i + Math.pow(2, h - 1)] + B[i];
 
         // Zad 2
         processorIndexToOperationsCount[i] = processorIndexToOperationsCount[i] ? processorIndexToOperationsCount[i] + 1: 1;
     }
     B = TEMP_B;
-    if (h === 3) console.log(`Krok ${h}`, B.toString());
+    if (h === 3 ) console.log(`Krok ${h}:`, B.toString());
 }
 
-console.log('Result');
-console.log(B);
-console.log(processorIndexToOperationsCount);
+// console.log('Result');
+// console.log(B);
+// console.log(processorIndexToOperationsCount);
 
 
 // Zad 1
@@ -55,3 +54,6 @@ console.log(processorIndexToOperationsCount);
 // Zad 3
 // Ile iteracji wykona pętla for dla danej tablicy o n elementach?
 
+// EREW PRAM
+// W(n)=O(nlogn)
+// T(n)=O(logn)
