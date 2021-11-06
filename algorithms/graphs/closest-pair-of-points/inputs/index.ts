8fb328f399816cb505f7b7e484f7f6920cbdd59e
Labus, Pawel <pawel.labus@falkbuilt.com>
2021-11-06 13:48:29 +0100

    Update

--- algorithms/graphs/closest-pair-of-points/inputs/index.ts
+++ algorithms/graphs/closest-pair-of-points/inputs/index.ts
@@ -1,3 +1,7 @@
 export * as input1 from './001';
 export * as input2 from './002';
 export * as input3 from './003';
+export * as input4 from './004';
+export * as input5 from './005';
+export * as input6 from './006';
+export * as input7 from './007';
--- algorithms/graphs/closest-pair-of-points/tests/closest-pair-of-points.spec.ts
+++ algorithms/graphs/closest-pair-of-points/tests/closest-pair-of-points.spec.ts
@@ -6,7 +6,15 @@ describe('closest pair of points', () => {
   it('should find closest pair of points', () => {
     const toPoints = (coords: number[]) => new Point(coords);
 
-    const data = [inputs.input1, inputs.input2, inputs.input3];
+    const data = [
+      inputs.input1,
+      inputs.input2,
+      inputs.input3,
+      inputs.input4,
+      inputs.input5,
+      inputs.input6,
+      inputs.input7,
+    ];
     data.forEach((testData) => {
       const points = testData.input.map(toPoints);
       const output = testData.output.map(toPoints);
