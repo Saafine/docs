2a5b6388bfb9f593c9d1640de1540c01f81a1f56
Labus, Pawel <pawel.labus@falkbuilt.com>
2021-12-10 16:45:35 +0100

    Update

--- algorithms/graphs/max-independent-set/max-independent-set.spec.ts
+++ algorithms/graphs/max-independent-set/max-independent-set.spec.ts
@@ -106,20 +106,110 @@ describe('max independent set', () => {
       ],
     };
 
-    function walk(testTree: TestTree, previous: NodeV) {
-      const node = new NodeV(testTree.node);
-      previous.addChild(node);
-      testTree.children?.forEach((child) => {
-        walk(child, node);
-      });
-    }
+    expect(test(tree)).toBe(14);
+  });
 
-    const start = new NodeV(tree.node);
-    walk(tree, start);
+  it('should work 2', () => {
+    const tree: TestTree = {
+      node: 1,
+      children: [
+        {
+          node: 11,
+          children: [
+            {
+              node: 111,
+            },
+            {
+              node: 112,
+            },
+          ],
+        },
+        {
+          node: 12,
+          children: [
+            {
+              node: 121,
+            },
+            {
+              node: 122,
+            },
+          ],
+        },
+      ],
+    };
 
-    const rootNode = start.getChildren()[0] as NodeV;
+    expect(test(tree)).toBe(5);
+  });
 
-    expect(rootNode.getMaxIndependentSetSize()).toBe(14);
-    // expect(rootNode.getTotalCalls()).toBe(13);
+  it('should work 3', () => {
+    const tree: TestTree = {
+      node: 1,
+      children: [
+        {
+          node: 2,
+          children: [
+            {
+              node: 4,
+              children: [
+                {
+                  node: 8,
+                  children: [
+                    {
+                      node: 13,
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+        {
+          node: 3,
+          children: [
+            {
+              node: 5,
+            },
+            {
+              node: 6,
+            },
+            {
+              node: 7,
+              children: [
+                {
+                  node: 9
+                },
+                {
+                  node: 10
+                },
+                {
+                  node: 11
+                },
+                {
+                  node: 12
+                },
+              ]
+            }
+          ]
+        },
+      ],
+    };
+
+    expect(test(tree)).toBe(9);
   });
 });
+
+function test(tree: TestTree): number {
+  function walk(testTree: TestTree, previous: NodeV) {
+    const node = new NodeV(testTree.node);
+    previous.addChild(node);
+    testTree.children?.forEach((child) => {
+      walk(child, node);
+    });
+  }
+
+  const start = new NodeV(tree.node);
+  walk(tree, start);
+
+  const rootNode = start.getChildren()[0] as NodeV;
+  return rootNode.getMaxIndependentSetSize();
+}
